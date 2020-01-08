7cd6469a7bb3b8e826d2744e1873ddcd44d96aba
aborgra <audrey.borgra@gmail.com>
2020-01-08 15:51:32 -0600

    added mood filter and search, not finished

--- scripts/filter/MoodFilter.js
+++ scripts/filter/MoodFilter.js
@@ -3,9 +3,9 @@ const eventHub = document.querySelector(".container")
 
 const MoodFilter = () => {
   return `
+    <div class="filterBar">
       <fieldset class="fieldset">
           <label for="filterMood">Filter Journal Entries by Mood:</label>
-          <div>
             <input type="radio" id="moodChoice1"
              name="moodFilter" value="Happy">
             <label for="contactChoice1">Happy</label>
@@ -18,14 +18,17 @@ const MoodFilter = () => {
             <input type="radio" id="moodChoice4"
               name="moodFilter" value="Frustrated">
             <label for="contactChoice4">Frustrated</label>
-          </div>
       </fieldset>
+      <fieldset class="fieldset">
+        <input type="search" id="searchField" placeholder="Search Journal Entries">
+      </fieldset>
+      </div>
       `
 }
 
 eventHub.addEventListener("click", event => {
-  console.log("radio button clicked")
   if (event.target.name === "moodFilter") {
+    console.log("radio button clicked")
       const mood = event.target.value
       console.log(event.target.value)
       const message = new CustomEvent("filterMoodChecked", {
@@ -34,6 +37,12 @@ eventHub.addEventListener("click", event => {
         }
       })
       eventHub.dispatchEvent(message)
+  }
+})
+
+eventHub.addEventListener ("keypress", event => {
+  if(event.keycode == 13 && document.querySelector("#searchField").innerHTML != "") {
+    console.log ("search entered")
   }
 })
 export default MoodFilter
